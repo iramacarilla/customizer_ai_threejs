6811c176b1eae4006ba46b482c87fef75e5f279a
Iryna <ikosynienko@gmail.com>
2023-05-26 13:03:19 +0200

    add code

---
+++ client/src/components/index.js
@@ -0,0 +1,7 @@
+import ColorPicker from './ColorPicker'
+import AIPicker from "./AIPicker";
+import CustomButton from "./CustomButton";
+import FilePicker from "./FilePicker";
+import Tab from "./Tab";
+
+export {CustomButton, AIPicker, ColorPicker, FilePicker, Tab};
