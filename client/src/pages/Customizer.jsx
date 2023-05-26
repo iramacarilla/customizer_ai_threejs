b2ebcdacb3806570c9cd8e42a4681a1b56c716c4
Iryna <ikosynienko@gmail.com>
2023-05-26 13:59:49 +0200

    change url

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -51,7 +51,7 @@ const Customizer = () => {
       //call out backend to generate an ai image
       setGeneratingImg(true)
 
-      const response = await fetch('http://localhost:8080/api/v1/dalle', {
+      const response = await fetch('https://customize-ai-threejs.onrender.com/api/v1/dalle', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
